6f57b1a19bd7e7bc8badc751dae3b1d216e0e43b
Kize <113998494+kize5@users.noreply.github.com>
2023-01-09 14:32:16 +0100

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,12 +6,12 @@
         <div class="conteneur-nav">
             <ul class="lst-nav">
                 <li class="myButton2"><a href="{{ url('/product') }}"
-                                         data-text="VR">VR</a></li>
+                                         data-text="VR">VR (prod)</a></li>
                 <li class="myButton2 bnt-travel"><a href="{{ url
         ('/product') }}"
-                                                    data-text="Voyage">Voyage</a></li>
+                                                    data-text="Voyage">Voyage (prod)</a></li>
                 <li class="myButton2"><a href="{{url('/product/{id}')}}"
-                                         data-text="Services">Services</a></li>
+                                         data-text="Services">Services (prod)</a></li>
             </ul>
         </div>
     </div>
@@ -19,8 +19,8 @@
 
 
     <div id="title">
-        <h1 id="name">eTravel</h1>
-        <h2 id="slogan">Explorer le monde autrement</h2>
+        <h1 id="name">eTravel v2.0.1</h1>
+        <h2 id="slogan">Explorer le monde autrement (prod)</h2>
     </div>
 
 @endsection
